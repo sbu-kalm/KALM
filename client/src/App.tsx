1af4c4df78b648af62c6ac0ef935dec8521e882f
Kayla Fang <kayfang182@gmail.com>
2023-11-04 22:38:33 -0400

    Started the dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from './components/pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Dashboard</h1>} />
+          <Route path="/" Component={Dashboard} />
           <Route path="/createFrame" element={<h1>About</h1>} />
           <Route path="/parseFrame" element={<h1>About</h1>} />
           <Route path="/training" element={<h1>About</h1>} />
--- client/src/components/pages/Dashboard.tsx
+++ client/src/components/pages/Dashboard.tsx
@@ -1,1 +1,57 @@
-export{};
+import { Title, Text, Button, Paper, Image, Flex } from '@mantine/core';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Title order={4} c="blue.4">Welcome to KALM</Title>
+            <Title order={1}>Translate your knowledge into logic</Title>
+            <Text size="md" fw={500} c="gray.6">sample sample sample sample sample sample sample</Text>
+            <Button variant="filled" radius="lg" size="sm" bg="blue.4">Get started!</Button>
+
+            <Title order={2} c="blue.4">Available Features</Title>
+            <Flex gap="md">
+                <Paper shadow="sm" p="md" withBorder>
+                    <Image
+                        src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                        height={160}
+                        alt="Norway"
+                    />
+                    <Text size="lg" fw={500} c="cyan.6">Parse Frames from User Text</Text>
+                    <Text>description of feature description of feature description of feature description of feature description </Text>
+                    <Button>Try Now</Button>
+                </Paper>
+                <Paper shadow="sm" p="md" withBorder>
+                    <Image
+                        src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                        height={160}
+                        alt="Norway"
+                    />
+                    <Text size="lg" fw={500} c="cyan.6">Parse Frames from User Text</Text>
+                    <Text>description of feature description of feature description of feature description of feature description </Text>
+                    <Button>Try Now</Button>
+                </Paper>
+                <Paper shadow="sm" p="md" withBorder>
+                    <Image
+                        src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                        height={160}
+                        alt="Norway"
+                    />
+                    <Text size="lg" fw={500} c="cyan.6">Parse Frames from User Text</Text>
+                    <Text>description of feature description of feature description of feature description of feature description </Text>
+                    <Button>Try Now</Button>
+                </Paper>
+                <Paper shadow="sm" p="md" withBorder>
+                    <Image
+                        src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
+                        height={160}
+                        alt="Norway"
+                    />
+                    <Text size="lg" fw={500} c="cyan.6">Parse Frames from User Text</Text>
+                    <Text>description of feature description of feature description of feature description of feature description </Text>
+                    <Button>Try Now</Button>
+                </Paper>
+            </Flex>
+        </div>
+    )
+}
+export default Dashboard;
