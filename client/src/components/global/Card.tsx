b99245ccacf12a3f9440898069c315fa35bf7fe5
Kayla Fang <kayfang182@gmail.com>
2023-11-04 23:13:02 -0400

    created card component

---
+++ client/src/components/global/Card.tsx
@@ -0,0 +1,26 @@
+import { Text, Button, Paper, Image } from '@mantine/core';
+
+interface CardProps {
+    img: string;
+    title: string;
+    description: string;
+    titleColor: string;
+    buttonColor: string;
+}
+
+const Card = (props: CardProps) => {
+    return (
+        <Paper style={{width: "330px"}} shadow="sm" p="md" withBorder>
+            <Image
+                src={props.img}
+                height={160}
+                alt="Norway"
+            />
+            <Text style={{padding: "10px 0px"}} size="lg" fw={500} c={props.titleColor}>{props.title}</Text>
+            <Text>{props.description}</Text>
+            <Button style={{margin: "10px 0px 0px 0px"}} bg={props.buttonColor} fullWidth>Try Now</Button>
+        </Paper>
+    )
+}
+
+export default Card;
