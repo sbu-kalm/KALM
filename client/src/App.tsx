067492a0ac75e00f704aebf2e3799e0ecef83ebd
jendyren <jendyren@gmail.com>
2023-11-06 01:34:34 -0500

    navbar styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
-import { AppShell, Burger, NavLink, ThemeIcon} from '@mantine/core';
+import { AppShell, Burger, NavLink, ThemeIcon, UnstyledButton, Group, Box, Stack } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { Home, Plus, Database, MessageSquare, HelpCircle, Disc, HardDrive } from 'react-feather';
 import Dashboard from './components/pages/Dashboard';
@@ -11,30 +11,37 @@ import Training from './components/pages/Training';
 import CleanFrame from './components/pages/CleanFrame';
 import QuestionAnswer from './components/pages/QuestionAnswer';
 import FAQ from './components/pages/FAQ';
+import "../src/css/NavbarStyle.css";
 
 function App() {
   const [opened, { toggle }] = useDisclosure();
   const [active, setActive] = useState(0);
   const navData = [
-    { icon: Home, label: 'Dashboard', route: '/'},
-    { icon: Plus, label: 'Create Frame', route: '/createFrame' },
-    { icon: Database, label: 'Parse Frame', route: '/parseFrame' },
-    { icon: HardDrive, label: 'Training', route: '/training'},
-    { icon: Disc, label: 'Clean Frame', route: '/cleanFrame'},
-    { icon: MessageSquare, label: 'Question Answer', route: '/questionAnswer'},
-    { icon: HelpCircle, label: 'FAQ', route: '/faq'},
+    { icon: Home, label: 'Dashboard', color: "blue", route: '/' },
+    { icon: Plus, label: 'Create Frame', color: "cyan", route: '/createFrame' },
+    { icon: Database, label: 'Parse Frame', color: "orange", route: '/parseFrame' },
+    { icon: HardDrive, label: 'Training', color: "grape", route: '/training' },
+    { icon: Disc, label: 'Clean Frame', color: "green", route: '/cleanFrame' },
+    { icon: MessageSquare, label: 'Question Answer', color: "yellow", route: '/questionAnswer' },
+    { icon: HelpCircle, label: 'FAQ', color: "red", route: '/faq' },
   ];
 
   const navItems = navData.map((item, index) => (
+    <>
       <NavLink
-        component={Link} 
+        component={Link}
         to={item.route}
         key={item.label}
         active={index === active}
         label={item.label}
-        leftSection={item.icon && <item.icon size={20} />}
+        leftSection={
+          <ThemeIcon variant="light" size={30} color={item.color}>
+            <item.icon size={20} />
+          </ThemeIcon>}
         onClick={() => setActive(index)}
+        variant="light"
       />
+    </>
   ));
 
   return (
@@ -48,7 +55,7 @@ function App() {
           <AppShell.Header>
             <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
             <div>
-              <h1 style={{margin:"0px 10px", color:"#339AF0"}}> KALM </h1>
+              <h1 style={{ margin: "0px 10px", color: "#339AF0" }}> KALM </h1>
             </div>
           </AppShell.Header>
 
@@ -58,13 +65,13 @@ function App() {
 
           <AppShell.Main>
             <Routes>
-              <Route path="/" element={<Dashboard/>} />
-              <Route path="/createFrame" element={<CreateFrame/>} />
-              <Route path="/parseFrame" element={<ParseFrame/>} />
-              <Route path="/training" element={<Training/>} />
-              <Route path="/cleanFrame" element={<CleanFrame/>} />
-              <Route path="/questionAnswer" element={<QuestionAnswer/>} />
-              <Route path="/faq" element={<FAQ/>} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/createFrame" element={<CreateFrame />} />
+              <Route path="/parseFrame" element={<ParseFrame />} />
+              <Route path="/training" element={<Training />} />
+              <Route path="/cleanFrame" element={<CleanFrame />} />
+              <Route path="/questionAnswer" element={<QuestionAnswer />} />
+              <Route path="/faq" element={<FAQ />} />
             </Routes>
           </AppShell.Main>
         </AppShell>
