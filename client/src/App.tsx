58e30f2e7965e645260edcd019237f94e80264e3
Zhen Wei Liao <zhenwei.liao@stonybrook.edu>
2025-04-21 10:54:32 -0400

    feat: implement brat visualization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,6 +18,7 @@ import Training from "./components/pages/Training/Training";
 import CleanPattern from "./components/pages/CleanPattern/CleanPattern";
 import QuestionAnswer from "./components/pages/QuestionAnswer";
 import FAQ from "./components/pages/FAQ";
+import BratVisualizer from "./components/pages/Brat";
 import "../src/css/NavbarStyle.css";
 import { ManageContextProvider } from "./context/ManageContextProvider";
 import { TrainingContextProvider } from "./context/TrainingContextProvider";
@@ -127,6 +128,7 @@ function App() {
             />
             <Route path="/questionAnswer" element={<QuestionAnswer />} />
             <Route path="/faq" element={<FAQ />} />
+            <Route path="/brat-test" element={<BratVisualizer />} />
           </Routes>
         </AppShell.Main>
       </AppShell>
